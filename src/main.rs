e96f0394ce076b951504525f21b397f2d8ce3495
satyam <satyam.mandal@intrcity.com>
2025-06-22 12:35:29 +0530

    rectange complete and basic of snake complete

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate piston_window; // import external Library
+extern crate rand;
+
+mod draw; // tell compiler we have another namespace  that we want to connect with main
+mod snake;
+
+fn main() {
+    println!("Hello, world!");
+}
